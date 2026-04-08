4b75328b3cf7cbb37be8a7f2550e01fbcdf1aaff
Omoju-Mayowa <omojumayowa@gmail.com>
2026-04-08 19:18:52 +0100

    Add .worktrees/ to .gitignore

---
+++ clicky/clickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickyApp.swift
+//  clicky
+//
+//  Created by Omoju Oluwamayowa Samuel on 08/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct clickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
